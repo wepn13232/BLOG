8fddb10a066ed2ac15fafc1690caa1dc7386272d
linqiyuan <466214222@qq.com>
2021-01-18 17:12:20 +0800

    更改部署git page参数

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@
 npm run build
 
 # 进入生成的文件夹
-cd /dist
+cd ./dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
- git push -f git@github.com:wepn13232/wepn13232.github.io.git master
+ git push -f https://github.com/wepn13232/wepn13232.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
